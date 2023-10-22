ab5b9db1c99fca2dde4fd4ed89498f93c2f2bd16
V1ctor9 <134568573+V1ctor9@users.noreply.github.com>
2023-10-22 12:58:48 -0500

    Update León.java

---
+++ León.java
@@ -0,0 +1,16 @@
+package Parcial2;
+
+public class León extends Felinos {
+    public León() {
+        hábitat = "pradera";
+        nombreCientífico = "Panthera leo";
+    }
+
+    public String getHábitat() {
+        return hábitat;
+    }
+
+    public String getNombreCientífico() {
+        return nombreCientífico;
+    }
+}
