6faf753f082bf097fbac3f655208f2b07a662c7f
V1ctor9 <134568573+V1ctor9@users.noreply.github.com>
2023-10-22 12:56:10 -0500

    Update Animal.java

---
+++ Animal.java
@@ -0,0 +1,14 @@
+abstract class Animal {
+    protected String sonidos;
+    protected String alimentos;
+    protected String hábitat;
+    protected String nombreCientífico;
+
+    public abstract String getNombreCientífico();
+
+    public abstract String getSonido();
+
+    public abstract String getAlimentos();
+
+    public abstract String getHábitat();
+}
