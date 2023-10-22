1bda7f8d5f626cc3a74a4b446a040e131766ea78
V1ctor9 <134568573+V1ctor9@users.noreply.github.com>
2023-10-22 13:00:24 -0500

    Update Perro.java

---
+++ Perro.java
@@ -0,0 +1,16 @@
+package Parcial2;
+
+public class Perro extends Cánidos {
+    public Perro() {
+        hábitat = "doméstico";
+        nombreCientífico = "Canis lupus familiaris";
+    }
+
+    public String getHábitat() {
+        return hábitat;
+    }
+
+    public String getNombreCientífico() {
+        return nombreCientífico;
+    }
+}
