b952bcf658f4581f4732f50df23d978d315b07dc
V1ctor9 <134568573+V1ctor9@users.noreply.github.com>
2023-10-22 12:58:20 -0500

    Update Gato.java

---
+++ Gato.java
@@ -0,0 +1,16 @@
+package Parcial2;
+
+public class Gato extends Felinos {
+    public Gato() {
+        hábitat = "doméstico";
+        nombreCientífico = "Felis silvestris catus";
+    }
+
+    public String getHábitat() {
+        return hábitat;
+    }
+
+    public String getNombreCientífico() {
+        return nombreCientífico;
+    }
+}
