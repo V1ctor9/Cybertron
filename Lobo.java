826cb276e193ef447c9dee4af3c6128f5e829fd1
V1ctor9 <134568573+V1ctor9@users.noreply.github.com>
2023-10-22 12:59:20 -0500

    Update Lobo.java

---
+++ Lobo.java
@@ -0,0 +1,16 @@
+package Parcial2;
+
+public class Lobo extends Cánidos {
+    public Lobo() {
+        hábitat = "bosque";
+        nombreCientífico = "Canis lupus";
+    }
+
+    public String getHábitat() {
+        return hábitat;
+    }
+
+    public String getNombreCientífico() {
+        return nombreCientífico;
+    }
+}
